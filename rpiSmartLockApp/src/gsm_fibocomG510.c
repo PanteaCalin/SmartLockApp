2d4b4b12124a9104912c309a5323392817ca30c1
PanteaCalin <calin.m.pantea@gmail.com>
2016-03-10 20:51:48 +0000

    [gsm_fibocomG510.c]:  - updated gsm_fibocomg510_init() with some more test-only operations(do not merge).  - split the debug print functions used between info and error printfs.

--- rpiSmartLockApp/src/gsm_fibocomG510.c
+++ rpiSmartLockApp/src/gsm_fibocomG510.c
@@ -38,62 +38,84 @@ static int gsm_fibocomg510_getOperatingMode();
 // global function definition
 // -----------------------------------------------------------
 int gsm_fibocomg510_init(void) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
 
     // init serial port
     gsmSerialPortFileDescriptor = uart.setupPortParams(GSM_UART_PORT, &serialPortCfg_Default);
     if (gsmSerialPortFileDescriptor < 0) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         return -1;
     }
     // turn on GSM module
     gsm_fibocomg510_turnOn();
+    sleepMs(GSM_HW_DELAY_AFTER_POWER_UP_MS);
 
     // verify GSM module power state
     if( gsm_fibocomg510_getPowerState() == VDD_POWER_OFF ) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         return -2;
     }
 
     // test-only
+    GSMFIBOCOMG510_PRINT_INFO("----------------------------------------------\n");
     gsm_fibocomg510_sendATcmd(GSM_AT_CMD_READ, GSM_SIGNAL_STRENGHT, NULL);
-    sleepMs(100);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
     gsm_fibocomg510_getATcmdResp(RETURN_FULL_RESP, NULL, NULL);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
 
-    sleepMs(100);
-
+     GSMFIBOCOMG510_PRINT_INFO("----------------------------------------------\n");
     gsm_fibocomg510_sendATcmd(GSM_AT_CMD_READ, GSM_SIM_PIN, NULL);
-    sleepMs(100);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
     gsm_fibocomg510_getATcmdResp(RETURN_FULL_RESP, NULL, NULL);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+
+    GSMFIBOCOMG510_PRINT_INFO("----------------------------------------------\n");
+    gsm_fibocomg510_sendATcmd(GSM_AT_CMD_SET, GSM_SIM_PIN, "\"0000\"");
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+    gsm_fibocomg510_getATcmdResp(RETURN_FULL_RESP, NULL, NULL);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+
+    GSMFIBOCOMG510_PRINT_INFO("----------------------------------------------\n");
+    gsm_fibocomg510_sendATcmd(GSM_AT_CMD_READ, GSM_SIM_PIN, NULL);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+    gsm_fibocomg510_getATcmdResp(RETURN_FULL_RESP, NULL, NULL);
+     sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+
+    sleepMs(1000);
+    gsm_fibocomg510_turnOff(TURN_OFF_SW);
+
+    // flush I/O UART buffers
+    sleepMs(GSM_HW_DELAY_BTWN_ATCMD_AND_RESP);
+    uart_flushInOutBuffers(gsmSerialPortFileDescriptor);
 
     gsm_fibocomg510_initialized = 1;
     return 0;
 }
 
 int gsm_fibocomg510_hw_check(void) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     return 0;
 }
 
 int gsm_fibocomg510_call(char* phoneNumber) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     return 0;
 }
 
 int gsm_fibocomg510_sendSMS(char* phoneNumber, char* sms) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     return 0;
 }
 
 int gsm_fibocomg510_getStatus(char *status) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     return 0;
 }
 
 // static function definition
 // -----------------------------------------------------------
 static int gsm_fibocomg510_sendATcmd(char* atCmdAction, char* atCmd, char* atCmdVal) {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
 
     char* atCmdPtr = NULL;
     int atCmdSize = 0;
@@ -101,13 +123,13 @@ static int gsm_fibocomg510_sendATcmd(char* atCmdAction, char* atCmd, char* atCmd
 
     // verify the validity of argument pointers
     if( (atCmdAction == NULL) || (atCmd == NULL) || ((atCmdAction == (char*)GSM_AT_CMD_SET) && (atCmdVal == NULL)) ) {
-       GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
        return -1;
     }
 
     // verify the validity of the global serial port file descriptor assigned to GSM module
     if(gsmSerialPortFileDescriptor < 0) {
-       GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
        return -2;
     }
 
@@ -131,7 +153,7 @@ static int gsm_fibocomg510_sendATcmd(char* atCmdAction, char* atCmd, char* atCmd
     atCmdPtr = malloc(atCmdSize);
 
     if (atCmdPtr == NULL) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         free(atCmdPtr);
         return -3;
     }
@@ -148,8 +170,8 @@ static int gsm_fibocomg510_sendATcmd(char* atCmdAction, char* atCmd, char* atCmd
     //snprintf(atCmdPtr, atCmdSize, "%s%s%s%s", GSM_AT_CMD_PREFIX, GSM_SIGNAL_STRENGHT, GSM_AT_CMD_READ, CR);
     //snprintf(atCmdPtr, atCmdSize, "%s%s%s%s", GSM_AT_CMD_PREFIX, atCmd, atCmdAction, CR);
 
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s - AT Cmd: %s\n",__func__, atCmdPtr);
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s - AT Cmd Size: %d\n", __func__, atCmdSize);
+    GSMFIBOCOMG510_PRINT_INFO("%s - AT Cmd: %s\n",__func__, atCmdPtr);
+    GSMFIBOCOMG510_PRINT_INFO("%s - AT Cmd Size: %d\n", __func__, atCmdSize);
 
     // send AT cmd using serial port
     bytesSend = uart.writeData(gsmSerialPortFileDescriptor, atCmdPtr, atCmdSize);
@@ -158,7 +180,7 @@ static int gsm_fibocomg510_sendATcmd(char* atCmdAction, char* atCmd, char* atCmd
     free(atCmdPtr);
 
     if(bytesSend < 0) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         return -4;
     }
     return bytesSend;
@@ -174,9 +196,9 @@ IMPORTANT: this function's design is very dangerous due to the dynamic allocatio
 static int gsm_fibocomg510_getATcmdResp(ty_ATCmdRespAction action, char* resp, char* expectedResp) {
 
     int uartInputBufferReceivedBytes = uart.getInputBytesAvailable(gsmSerialPortFileDescriptor);
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s - INFO: uart input buffer bytes available: %d\n",__func__, uartInputBufferReceivedBytes);
+    GSMFIBOCOMG510_PRINT_INFO("%s - INFO: uart input buffer bytes available: %d\n",__func__, uartInputBufferReceivedBytes);
     if(uartInputBufferReceivedBytes <= 0) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         return -1;
     }
 
@@ -186,20 +208,20 @@ static int gsm_fibocomg510_getATcmdResp(ty_ATCmdRespAction action, char* resp, c
     char* atCmdRespPtr = malloc(uartInputBufferReceivedBytes * sizeof(char));
 
     if(atCmdRespPtr == NULL) {
-        GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+        GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
         free(atCmdRespPtr);
         return -2;
     }
 
     // read AT Cmd Response
     uart.readInputBuffer(gsmSerialPortFileDescriptor, atCmdRespPtr);
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s - INFO: AT Cmd Response: \"%s\"\n",__func__, atCmdRespPtr);
+    GSMFIBOCOMG510_PRINT_INFO("%s - INFO: AT Cmd Response: \"%s\"\n",__func__, atCmdRespPtr);
 
     // parse the response depending on the action demanded
     switch(action) {
         case RETURN_FULL_RESP:
             if(resp == NULL) {
-                GSMFIBOCOMG510_DGB_PRINT_MSG("ERROR: file %s, line %d\n",__FILE__, __LINE__);
+                GSMFIBOCOMG510_PRINT_ERROR("ERROR: file %s, line %d\n",__FILE__, __LINE__);
                 free(atCmdRespPtr);
                 return -3;
             }
@@ -230,17 +252,18 @@ static ty_vdd_power_status gsm_fibocomg510_getPowerState() {
 }
 
 static void gsm_fibocomg510_turnOff(ty_turnOff_mode mode) {
-
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     switch(mode) {
     case TURN_OFF_HW:
         gpio.setDir(GSM_FIBOCOMG510_GPIO_POWER_ON, GPIO_DIR_OUT);
+        gpio.setLvlHigh(GSM_FIBOCOMG510_GPIO_POWER_ON); // just to make sure there's a negative edge
         gpio.setLvlLow(GSM_FIBOCOMG510_GPIO_POWER_ON);
         sleepMs(GSM_HW_POWER_DOWN_DELAY_MS);
         gpio.setLvlHigh(GSM_FIBOCOMG510_GPIO_POWER_ON);
         break;
 
     case TURN_OFF_SW:
-        gsm_fibocomg510_sendATcmd(GSM_AT_CMD_SET, GSM_HW_POWER_DOWN, NULL);
+        gsm_fibocomg510_sendATcmd(GSM_AT_CMD_SET_NO_VALUE, GSM_HW_POWER_DOWN, NULL);
         break;
 
     default:
@@ -249,15 +272,16 @@ static void gsm_fibocomg510_turnOff(ty_turnOff_mode mode) {
 }
 
 static void gsm_fibocomg510_turnOn() {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     gpio.setDir(GSM_FIBOCOMG510_GPIO_POWER_ON, GPIO_DIR_OUT);
+    gpio.setLvlHigh(GSM_FIBOCOMG510_GPIO_POWER_ON); // just to make sure there's a negative edge
     gpio.setLvlLow(GSM_FIBOCOMG510_GPIO_POWER_ON);
     sleepMs(GSM_HW_POWER_UP_DELAY_MS);
     gpio.setLvlHigh(GSM_FIBOCOMG510_GPIO_POWER_ON);
 }
 
 static void gsm_fibocomg510_reset() {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     gpio.setDir(GSM_FIBOCOMG510_GPIO_RST, GPIO_DIR_OUT);
     gpio.setLvlLow(GSM_FIBOCOMG510_GPIO_RST);
     sleepMs(GSM_HW_RESET_DELAY_MS);
@@ -265,7 +289,7 @@ static void gsm_fibocomg510_reset() {
 }
 
 static void gsm_fibocomg510_wakeUp() {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     gpio.setDir(GSM_FIBOCOMG510_GPIO_WAKE_UP, GPIO_DIR_OUT);
     gpio.setLvlLow(GSM_FIBOCOMG510_GPIO_WAKE_UP);
     sleepMs(GSM_HW_WAKEUP_DELAY_MS);
@@ -273,7 +297,7 @@ static void gsm_fibocomg510_wakeUp() {
 }
 
 static int gsm_fibocomg510_getOperatingMode() {
-    GSMFIBOCOMG510_DGB_PRINT_MSG("%s\n", __func__);
+    GSMFIBOCOMG510_PRINT_INFO("%s\n", __func__);
     //TODO
     return 0;
 }
